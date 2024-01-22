17732a7b85ad69df20b8ecc0075ec3c27c7a84d4
GuiMedeirox <omaewa9111@gmail.com>
2024-01-22 15:10:11 -0300

    feat: getters & setters with lombok (#1)

--- src/main/java/com/bicho/jogodobicho/model/Usuario.java
+++ src/main/java/com/bicho/jogodobicho/model/Usuario.java
@@ -5,11 +5,17 @@ import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
 import org.hibernate.annotations.CreationTimestamp;
 import org.hibernate.annotations.UpdateTimestamp;
 
 import java.time.LocalDateTime;
 
+@Getter
+@Setter
+@AllArgsConstructor
 @Entity
 @Table(name="usuario")
 public class Usuario {
