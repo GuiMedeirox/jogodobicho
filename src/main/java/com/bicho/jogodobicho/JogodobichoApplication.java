bb28d8f152636ec32cca175619a4c2d17b3fd05e
GuiMedeirox <omaewa9111@gmail.com>
2024-01-22 09:59:09 -0300

    first commit

---
+++ src/main/java/com/bicho/jogodobicho/JogodobichoApplication.java
@@ -0,0 +1,13 @@
+package com.bicho.jogodobicho;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JogodobichoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JogodobichoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bicho/jogodobicho/JogodobichoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bicho.jogodobicho;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JogodobichoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
